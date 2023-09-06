e766ff6e6a9ba1bcef7a322ae4dc378b645597a0
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-09-07 00:41:10 +0300

    add route with filters and mocked todo list

--- src/app.py
+++ src/app.py
@@ -8,6 +8,29 @@ class ToDoItems:
     done: bool
 
 
+TODO_LIST: list[ToDoItems] = [
+    ToDoItems(title="first task", done=False),
+    ToDoItems(title="second task", done=True),
+    ToDoItems(title="third task", done=False),
+]
+
+
+@get(
+    path="/todo",
+    tags=["todo"],
+    summary="Get list of ToDo items",
+    description="This route returns list of ToDo items",
+)
+async def get_todo_list(done: bool|None = None) -> list[ToDoItems]:
+    if done is None:
+        return TODO_LIST
+    elif done:
+        return [item for item in TODO_LIST if item.done]
+    else:
+        return [item for item in TODO_LIST if not item.done]
+
+
+
 @get(
     path="/hello-world",
     tags=["main"],
@@ -19,5 +42,8 @@ async def hello_world() -> str:
 
 
 app = Litestar(
-    route_handlers=[hello_world]
+    route_handlers=[
+        hello_world,
+        get_todo_list,
+    ]
 )
