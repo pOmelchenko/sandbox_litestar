707366863908480b94c28c6257afca31ee7c5453
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-09-08 20:07:54 +0300

    rename dataclass name

--- src/todo.py
+++ src/todo.py
@@ -4,15 +4,15 @@ from litestar import get
 
 
 @dataclass
-class ToDoItems:
+class ToDoItem:
     title: str
     done: bool
 
 
-TODO_LIST: list[ToDoItems] = [
-    ToDoItems(title="first task", done=False),
-    ToDoItems(title="second task", done=True),
-    ToDoItems(title="third task", done=False),
+TODO_LIST: list[ToDoItem] = [
+    ToDoItem(title="first task", done=False),
+    ToDoItem(title="second task", done=True),
+    ToDoItem(title="third task", done=False),
 ]
 
 
@@ -22,7 +22,7 @@ TODO_LIST: list[ToDoItems] = [
     summary="Get list of ToDo items",
     description="This route returns list of ToDo items",
 )
-async def get_todo_list(done: bool | None = None) -> list[ToDoItems]:
+async def get_todo_list(done: bool | None = None) -> list[ToDoItem]:
     if done is None:
         return TODO_LIST
     return [item for item in TODO_LIST if item.done == done]
